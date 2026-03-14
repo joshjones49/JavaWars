891614094d0683c121453377f5ac1f5ffe11cc96
Joshua Jones <143196112+joshjones49@users.noreply.github.com>
2026-03-14 19:42:48 -0400

    Created a few basic components of the UI

--- main/src/main/java/App.java
+++ main/src/main/java/App.java
@@ -3,10 +3,36 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class App extends JFrame {
-    // create app components from Swing library
+    // create app components from Swing
+    // JPanels
     private final JPanel rootPanel = new JPanel(new BorderLayout(10, 10));
-    private final JLabel titleLabel = new JLabel("No Problem Loaded");
     private final JPanel topBarPanel = new JPanel(new BorderLayout());
+    private final JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+    // JLabels
+    private final JLabel titleLabel = new JLabel("No Problem Loaded");
+
+    // JButtons
+    private final JButton randomButton = new JButton("Random Problem");
+    private final JButton resetButton = new JButton("Reset Starter");
+    private final JButton hintButton = new JButton("Hint");
+    private final JButton checkButton = new JButton("Check Solution");
+
+    // JTextAreas
+    private final JTextArea resultArea = new JTextArea();
+    private final JTextArea problemArea = new JTextArea();
+
+    // JComboBoxs
+    private final JComboBox<String> fontComboBox = new JComboBox<>();
+
+    // JScrollPanes
+    // puts problemArea and resultArea into scrollable container
+    private final JScrollPane problemScrollPane = new JScrollPane(problemArea);
+    private final JScrollPane resultScrollPane = new JScrollPane(resultArea);
+
+    // JSplitPanes
+    private final JSplitPane verticalSplit = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+    private final JSplitPane bottomSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
